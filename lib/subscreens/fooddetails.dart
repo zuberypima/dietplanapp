f95c9db6850b024ed4bac1a6d38764191a92cffc
zuberypim <zuberypima12@gmail.com>
2022-07-07 16:16:13 +0300

    New smartdiet app

--- lib/subscreens/fooddetails.dart
+++ lib/subscreens/fooddetails.dart
@@ -1,15 +1,74 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class FoodDetails extends StatefulWidget {
-  const FoodDetails({ Key? key }) : super(key: key);
+  const FoodDetails({Key? key}) : super(key: key);
 
   @override
   State<FoodDetails> createState() => _FoodDetailsState();
 }
 
 class _FoodDetailsState extends State<FoodDetails> {
+  final Stream<QuerySnapshot> _foodStream = FirebaseFirestore.instance
+      .collection('DeitPlanData')
+      .doc('LunchFoods')
+      .collection('Proteinfoods')
+      .snapshots();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('data'),),);
+    return Scaffold(
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.orange[200],
+        ),
+        body: ListView(children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+            child: Container(
+              height: MediaQuery.of(context).size.height / 1.2,
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(15)),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 15),
+                child: StreamBuilder<QuerySnapshot>(
+                    stream: _foodStream,
+                    builder: (BuildContext context,
+                        AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.hasError) {
+                        return Text('Something went wrong');
+                      }
+
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Text("Loading");
+                      }
+
+                      return ListView(
+                        children: snapshot.data!.docs
+                            .map((DocumentSnapshot document) {
+                          Map<String, dynamic> data =
+                              document.data()! as Map<String, dynamic>;
+                          return Column(
+                            children: [
+                              ListTile(
+                                leading: Container(
+                                  height: 70,
+                                  width: 70,
+                                  decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      borderRadius: BorderRadius.circular(10)),
+                                ),
+                                title: Text('${data['Name']}'),
+                              ),
+                              Divider(thickness: 3,)
+                            ],
+                          );
+                        }).toList(),
+                      );
+                    }),
+              ),
+            ),
+          ),
+        ]));
   }
 }
